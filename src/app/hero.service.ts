4d68cb8262b5a6eec47f2697e45e0c016540f83e
Ruben Scholle <ruben.scholle@outlook.com>
2020-10-25 09:53:22 +0100

    Added routable HeroDetailComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-// Annotation to register the service with the injector
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  /* This only works now, because the heroes come from a mock list.
-     In case of data from servers, the operation becomes asynchronous (?).
-     The browser does not wait/blocks for the server to respond. The
-     service must be asynchronous, therefore.
-  */
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
