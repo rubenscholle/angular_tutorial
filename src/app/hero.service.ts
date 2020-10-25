2319bb1b9da9cb6fcc502d2c8ad4ccc453effbbb
Ruben Scholle <ruben.scholle@outlook.com>
2020-10-25 09:29:05 +0100

    Added a MessageService and MessageComponent

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 // Annotation to register the service with the injector
 @Injectable({
@@ -9,15 +10,17 @@ import { Observable, of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   /* This only works now, because the heroes come from a mock list.
      In case of data from servers, the operation becomes asynchronous (?).
      The browser does not wait/blocks for the server to respond. The
      service must be asynchronous, therefore.
   */
- getHeroes(): Observable<Hero[]> {
-  return of(HEROES);
-}
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
 
 }
